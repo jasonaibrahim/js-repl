9de48ce01d8f70b0d3575c1eaf5ec2466af66c13
Jason Ibrahim <jasonaibrahim@gmail.com>
2022-10-22 12:53:40 -0700

    functional repl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
+
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Recursive", "monospace"].join(","),
+  },
+  components: {},
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,36 @@
-#![cfg_attr(
-all(not(debug_assertions), target_os = "windows"),
-windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+
+use deno_core::JsRuntime;
+use deno_core::RuntimeOptions;
+use deno_core::v8;
+
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![eval])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder
+        ::default()
+        .invoke_handler(tauri::generate_handler![eval])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn eval(code: &str) -> String {
-  format!("Nice: {}", code)
+fn eval(code: &str) -> Result<serde_json::Value, String> {
+    let mut runtime = JsRuntime::new(RuntimeOptions::default());
+
+    let res = runtime.execute_script("<anon>", code);
+    match res {
+        Ok(global) => {
+            let scope = &mut runtime.handle_scope();
+            let local = v8::Local::new(scope, global);
+            // Deserialize a `v8` object into a Rust type using `serde_v8`,
+            // in this case deserialize to a JSON `Value`.
+            let deserialized_value = serde_v8::from_v8::<serde_json::Value>(scope, local);
+
+            match deserialized_value {
+                Ok(value) => Ok(value),
+                Err(err) => Err(format!("Cannot deserialize value: {:?}", err)),
+            }
+        }
+        Err(err) => Err(format!("{:?}", err)),
+    }
 }
